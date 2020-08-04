51eb21dd068dc643da1b8bc5098b19a1d11a6930
Isaac Gorman <emailme@isaacg.net>
2020-08-04 08:15:12 -0500

    Intro notes complete. Up next => Three phases of the React Component Lifecycle

---
+++ lifecycle-practice/src/📔notes/Overview.js
@@ -0,0 +1,13 @@
+// THE REACT LIFECYCLE
+
+// Module Summary:
+// - Every React component contains what is known as the React Lifecycle.
+// - This React Lifecycle is just one piece of how components operate in vanilla JS.
+// - The React Lifecyle contains a few magical "Methods" on it, and learning how to use them, will help me understand why the React library behaves and acts the way it does at certain times.
+
+// Module Learning Objectives:
+// 1) Model and graph the three stages of the React Component Lifecycle
+// 2) Ablitly to define two important methods in the React Component Lifecycle and be able to explain their significance in the lifecycle:
+// -- "constructor"
+// -- "render"
+// 3) Render a component's state using the 'componentDidMount()' function.
