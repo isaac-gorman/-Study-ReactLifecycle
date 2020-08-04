3370c9a6eb60d65417fff81fe0483c7772d0ef89
Isaac Gorman <emailme@isaacg.net>
2020-08-04 08:00:18 -0500

    react app created, trimmed the fat, added prettier config. Up next => ready for TK study

---
+++ lifecycle-practice/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h6>React Lifecycle</h6>
+      </header>
+    </div>
+  );
+}
+
+export default App;
